92fbf718b7ea4de2bf2bfeb259ea6637379c40f8
Niklas Kuuva <niklas.kuuva@gmail.com>
2026-04-24 08:59:12 +0300

    Dual-stage bootloader. BROKEN: Will compile and boot, will fail at starting PMM backend

--- Makefile
+++ Makefile
@@ -1,18 +1,19 @@
-BOOT_SRC      = boot.asm
-ENTRY_SRC     = entry.asm
-ISR_IRQ_SRC   = isr_irq.asm
-LINKER_SRC    = linker.ld
+BOOT_SRC        = boot.asm
+LOADER2_SRC     = loader2.asm
+ENTRY_SRC       = entry.asm
+LINKER_SRC      = linker.ld
 
-C_SOURCES     = $(filter-out ,$(wildcard *.c))
+C_SOURCES       = $(wildcard *.c)
 
-BOOT_BIN      = boot.bin
-ENTRY_OBJ     = entry.o
-ISR_IRQ_OBJ   = isr_irq.o
-KERNEL_OBJS   = $(C_SOURCES:.c=.o)
-KERNEL_ELF    = kernel.elf
-KERNEL_BIN    = kernel.bin
+BOOT_BIN        = boot.bin
+LOADER2_BIN     = loader2.bin
+ENTRY_OBJ       = entry.o
+KERNEL_OBJS     = $(C_SOURCES:.c=.o)
+KERNEL_ELF      = kernel.elf
+KERNEL_BIN      = kernel.bin
+OS_IMG          = os.img
+
 KERNEL_SECTORS_INC = kernel_sectors.inc
-OS_IMG        = os.img
 
 NASM        = nasm
 CC          = x86_64-elf-gcc
@@ -22,24 +23,21 @@ DD          = dd
 
 SECTOR_SIZE        = 512
 IMG_SECTORS        = 2880
-KERNEL_MAX_SECTORS = 128
+KERNEL_MAX_SECTORS = 256
 
 CFLAGS  = -m32 -march=i386 -ffreestanding -fno-pic -fno-pie -fno-stack-protector -fno-asynchronous-unwind-tables -fno-unwind-tables -nostdlib -O2 -Wall -Wextra
 LDFLAGS = -m elf_i386 -T $(LINKER_SRC) -nostdlib
 
 all: $(OS_IMG)
 
-$(ENTRY_OBJ): $(ENTRY_SRC)
-	$(NASM) -f elf32 $(ENTRY_SRC) -o $(ENTRY_OBJ)
-
-$(ISR_IRQ_OBJ): $(ISR_IRQ_SRC)
-	$(NASM) -f elf32 $(ISR_IRQ_SRC) -o $(ISR_IRQ_OBJ)
-
 %.o: %.c
 	$(CC) $(CFLAGS) -c $< -o $@
 
-$(KERNEL_ELF): $(ENTRY_OBJ) $(ISR_IRQ_OBJ) $(KERNEL_OBJS) $(LINKER_SRC)
-	$(LD) $(LDFLAGS) -o $(KERNEL_ELF) $(ENTRY_OBJ) $(ISR_IRQ_OBJ) $(KERNEL_OBJS)
+$(ENTRY_OBJ): $(ENTRY_SRC)
+	$(NASM) -f elf32 $(ENTRY_SRC) -o $(ENTRY_OBJ)
+
+$(KERNEL_ELF): $(ENTRY_OBJ) $(KERNEL_OBJS) $(LINKER_SRC)
+	$(LD) $(LDFLAGS) -o $(KERNEL_ELF) $(ENTRY_OBJ) $(KERNEL_OBJS)
 
 $(KERNEL_BIN): $(KERNEL_ELF)
 	$(OBJCOPY) -O binary $(KERNEL_ELF) $(KERNEL_BIN)
@@ -55,16 +53,20 @@ $(KERNEL_SECTORS_INC): $(KERNEL_BIN)
 	sectors=$$(( ($$size + $(SECTOR_SIZE) - 1) / $(SECTOR_SIZE) )); \
 	echo "KERNEL_SECTORS equ $$sectors" > $(KERNEL_SECTORS_INC)
 
-$(BOOT_BIN): $(BOOT_SRC) $(KERNEL_SECTORS_INC)
+$(LOADER2_BIN): $(LOADER2_SRC) $(KERNEL_SECTORS_INC)
+	$(NASM) -f bin $(LOADER2_SRC) -o $(LOADER2_BIN)
+
+$(BOOT_BIN): $(BOOT_SRC)
 	$(NASM) -f bin $(BOOT_SRC) -o $(BOOT_BIN)
 
-$(OS_IMG): $(BOOT_BIN) $(KERNEL_BIN)
+$(OS_IMG): $(BOOT_BIN) $(LOADER2_BIN) $(KERNEL_BIN)
 	$(DD) if=/dev/zero of=$(OS_IMG) bs=$(SECTOR_SIZE) count=$(IMG_SECTORS)
 	$(DD) if=$(BOOT_BIN) of=$(OS_IMG) conv=notrunc
-	$(DD) if=$(KERNEL_BIN) of=$(OS_IMG) bs=$(SECTOR_SIZE) seek=1 conv=notrunc
+	$(DD) if=$(LOADER2_BIN) of=$(OS_IMG) bs=$(SECTOR_SIZE) seek=1 conv=notrunc
+	$(DD) if=$(KERNEL_BIN) of=$(OS_IMG) bs=$(SECTOR_SIZE) seek=9 conv=notrunc
 
 run: $(OS_IMG)
 	qemu-system-i386 -fda $(OS_IMG) -boot a
 
 clean:
-	rm -f $(BOOT_BIN) $(ENTRY_OBJ) $(ISR_IRQ_OBJ) $(KERNEL_OBJS) $(KERNEL_ELF) $(KERNEL_BIN) $(KERNEL_SECTORS_INC) $(OS_IMG)
+	rm -f $(BOOT_BIN) $(LOADER2_BIN) $(ENTRY_OBJ) $(KERNEL_OBJS) $(KERNEL_ELF) $(KERNEL_BIN) $(KERNEL_SECTORS_INC) $(OS_IMG)
--- boot.asm
+++ boot.asm
@@ -1,26 +1,13 @@
 [org 0x7C00]
 bits 16
 
-TEMP_KERNEL_SEG      equ 0x1000
-TEMP_KERNEL_ADDR     equ 0x00010000
-KERNEL_PHYS_ADDR     equ 0x00100000
-KERNEL_VIRT_BASE     equ 0xC0000000
-KERNEL_VIRT_ENTRY    equ 0xC0100000
+STAGE2_SEG        equ 0x0800
+STAGE2_OFF        equ 0x0000
 
-SECTORS_PER_TRACK    equ 18
-HEADS_PER_CYLINDER   equ 2
+STAGE2_SECTORS equ 8
 
-E820_INFO_ADDR    equ 0x0500
-E820_MAX_ENTRIES  equ 32
-E820_SIG          equ 0x324F3845
-
-PAGE_DIR_PHYS        equ 0x00009000
-PAGE_TABLE0_PHYS     equ 0x0000A000
-
-CODE_SEL             equ 0x08
-DATA_SEL             equ 0x10
-
-%include "kernel_sectors.inc"
+SECTORS_PER_TRACK equ 18
+HEADS_PER_CYLINDER equ 2
 
 start:
     cli
@@ -28,30 +15,30 @@ start:
     mov ds, ax
     mov es, ax
 
-    mov ax, 0x8000
+    mov ax, 0x9000
     mov ss, ax
     mov sp, 0xFFFF
 
     mov [boot_drive], dl
 
-    ; debug A
+    ; debug: '1'
     mov ax, 0xB800
     mov gs, ax
-    mov word [gs:0], 0x0741
+    mov word [gs:0], 0x0731
 
-    ; load kernel temporarily to 0x1000:0000 = 0x00010000
-    mov ax, TEMP_KERNEL_SEG
+    ; load stage 2 to 0x0800:0000 = 0x00008000
+    mov ax, STAGE2_SEG
     mov es, ax
     xor bx, bx
 
     mov byte [curr_sector], 2
     mov byte [curr_head], 0
     mov byte [curr_cylinder], 0
-    mov word [sectors_left], KERNEL_SECTORS
+    mov word [sectors_left], STAGE2_SECTORS
 
-load_kernel:
+.load_stage2:
     cmp word [sectors_left], 0
-    je load_done
+    je .done
 
     mov ah, 0x02
     mov al, 0x01
@@ -59,7 +46,6 @@ load_kernel:
     mov cl, [curr_sector]
     mov dh, [curr_head]
     mov dl, [boot_drive]
-
     int 0x13
     jc disk_error
 
@@ -68,36 +54,28 @@ load_kernel:
 
     inc byte [curr_sector]
     cmp byte [curr_sector], SECTORS_PER_TRACK + 1
-    jne load_kernel
+    jne .load_stage2
 
     mov byte [curr_sector], 1
     inc byte [curr_head]
     cmp byte [curr_head], HEADS_PER_CYLINDER
-    jne load_kernel
+    jne .load_stage2
 
     mov byte [curr_head], 0
     inc byte [curr_cylinder]
-    jmp load_kernel
+    jmp .load_stage2
 
-load_done:
-    ; debug B
-    mov ax, 0xB800
-    mov gs, ax
-    mov word [gs:2], 0x0742
+.done:
+    ; pass boot drive in DL
+    mov dl, [boot_drive]
 
-    cli
-    lgdt [gdt_descriptor]
-
-    mov eax, cr0
-    or eax, 1
-    mov cr0, eax
-
-    jmp CODE_SEL:protected_mode
+    ; far jump to stage 2
+    jmp STAGE2_SEG:STAGE2_OFF
 
 disk_error:
     mov ax, 0xB800
     mov gs, ax
-    mov word [gs:4], 0x0745
+    mov word [gs:2], 0x0745   ; 'E'
 .hang:
     cli
     hlt
@@ -108,160 +86,6 @@ curr_sector   db 0
 curr_head     db 0
 curr_cylinder db 0
 sectors_left  dw 0
-
-gdt_start:
-    dq 0
-
-gdt_code:
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 10011010b
-    db 11001111b
-    db 0x00
-
-gdt_data:
-    dw 0xFFFF
-    dw 0x0000
-    db 0x00
-    db 10010010b
-    db 11001111b
-    db 0x00
-
-gdt_end:
-
-gdt_descriptor:
-    dw gdt_end - gdt_start - 1
-    dd gdt_start
-
-collect_e820:
-    pushad
-    push es
-    push di
-
-    xor ax, ax
-    mov es, ax
-
-    mov di, E820_INFO_ADDR
-    mov dword [es:di], E820_SIG
-    mov word  [es:di+4], 0
-    mov word  [es:di+6], 0
-
-    mov di, E820_INFO_ADDR + 8
-    xor ebx, ebx
-
-.e820_loop:
-    mov eax, 0xE820
-    mov edx, 0x534D4150
-    mov ecx, 24
-    mov dword [es:di+20], 1
-    int 0x15
-    jc .done
-
-    cmp eax, 0x534D4150
-    jne .done
-
-    mov eax, [es:di+8]
-    or  eax, [es:di+12]
-    jz .skip_store
-
-    inc word [es:E820_INFO_ADDR + 4]
-    add di, 24
-
-    cmp word [es:E820_INFO_ADDR + 4], E820_MAX_ENTRIES
-    jae .done
-
-.skip_store:
-    test ebx, ebx
-    jne .e820_loop
-
-.done:
-    pop di
-    pop es
-    popad
-    ret
-
-bits 32
-protected_mode:
-    mov ax, DATA_SEL
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov esp, 0x90000
-
-    ; debug P
-    mov word [0xB8004], 0x0750
-
-    ; copy kernel from 0x00010000 to 0x00100000
-    mov esi, TEMP_KERNEL_ADDR
-    mov edi, KERNEL_PHYS_ADDR
-    mov ecx, KERNEL_SECTORS
-    shl ecx, 9                  ; sectors * 512
-    rep movsb
-
-    ; clear page directory
-    mov edi, PAGE_DIR_PHYS
-    mov ecx, 1024
-    xor eax, eax
-    rep stosd
-
-    ; clear page table
-    mov edi, PAGE_TABLE0_PHYS
-    mov ecx, 1024
-    xor eax, eax
-    rep stosd
-
-    ; identity-map first 4 MiB
-    mov edi, PAGE_TABLE0_PHYS
-    xor ebx, ebx
-    mov ecx, 1024
-.map_loop:
-    mov eax, ebx
-    or eax, 0x003              ; present + writable
-    mov [edi], eax
-    add ebx, 0x1000
-    add edi, 4
-    loop .map_loop
-
-    ; PDE[0] = page table for identity map
-    mov eax, PAGE_TABLE0_PHYS
-    or eax, 0x003
-    mov [PAGE_DIR_PHYS + 0*4], eax
-
-    ; PDE[768] = same page table mapped at 0xC0000000
-    mov eax, PAGE_TABLE0_PHYS
-    or eax, 0x003
-    mov [PAGE_DIR_PHYS + 768*4], eax
-
-    ; load page directory
-    mov eax, PAGE_DIR_PHYS
-    mov cr3, eax
-
-    ; enable paging
-    mov eax, cr0
-    or eax, 0x80000000
-    mov cr0, eax
-
-    ; far jump flush
-    jmp CODE_SEL:higher_half_entry
-
-higher_half_entry:
-    ; now executing with paging on
-    mov ax, DATA_SEL
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    mov ss, ax
-    mov esp, 0xC0090000
-
-    ; debug H
-    mov word [0xC00B8006], 0x0748
-
-    mov eax, KERNEL_VIRT_ENTRY
-    jmp eax
 
 times 510-($-$$) db 0
 dw 0xAA55
